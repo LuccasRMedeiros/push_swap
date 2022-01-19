2e9b432e24b3a9b1d1a4603f613149b6c98c78d0
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2022-01-19 18:08:48 -0300

    corrected bug

--- core/psc_simplesort.c
+++ core/psc_simplesort.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 20:50:14 by lrocigno          #+#    #+#             */
-/*   Updated: 2022/01/19 11:03:49 by lrocigno         ###   ########.fr       */
+/*   Updated: 2022/01/19 18:06:57 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,10 @@ void	psc_simplesort(t_prog *prog)
 	b = prog->stack_b;
 	while (pscu_observe(a, pscu_flwnx))
 	{
-		if (a->rank == 2 && a->next->rank == 1 && b->next->rank == 3)
+		if (((a->rank == 1 && a->prev->rank == 2)
+			|| (a->rank == 1 && a->prev->rank == 0)) && prog->a_size <= 3)
+			pscm_rra(&prog, &a);
+		else if (a->rank == 2 && a->next->rank == 1 && b->next->rank == 3)
 			pscm_ss(&prog, &a);
 		else if (a->rank == 2 && a->next->rank == 1)
 			pscm_sa(&prog, &a);
